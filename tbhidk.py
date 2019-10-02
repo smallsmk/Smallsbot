bbc8c8d691fade5f2b2ec1a39f0c507879cc97e1
smallsmk <ibarramiguel01@gmail.com>
2019-10-01 20:40:40 -0700

    first commit

---
+++ tbhidk.py
@@ -0,0 +1,95 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+from discord.utils import get
+import youtube_dl
+import random
+import ffmpeg
+import os
+
+TOKEN = 'NjI4MzI4ODE1NzAxMjYyMzU2.XZKLOw.VIy8jYf6oxJeABwtwISqBgr_LHg'
+BOT_PREFIX = '!'
+
+bot: Bot = commands.Bot(command_prefix=BOT_PREFIX)
+
+
+@bot.event
+async def on_ready():
+    print("Logged in as: " + bot.user.name + "\n")
+
+
+@bot.command(pass_context=True, aliases=['8Ball', '8ball', 'q', 'Q', 'ask'])
+async def _8ball(ctx, *, question):
+    responses = ['idk', 'maybe', 'yes', 'no', 'im just a bot ok?', 'im not your mom', 'f"yolo"']
+    await ctx.send(f'Answer: {random.choice(responses)}')
+
+
+@bot.command(pass_context=True, aliases=['j'])
+async def join(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+
+
+@bot.command(pass_context=True, aliases=['l', 'lea'])
+async def leave(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.disconnect()
+        print(f"The bot has left {channel}")
+    else:
+        print("Bot was told to leave voice channel, but was not in one")
+        await ctx.send("I'm not in a channel ;A;")
+
+
+@bot.command(pass_context=True, aliases=['p', 'pla'])
+async def play(ctx, url: str):
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+            print("Removed old song file")
+    except PermissionError:
+        print("Trying to delete song file, but it's being played")
+        await ctx.send("ERROR: Music playing")
+        return
+
+    await ctx.send("Getting everything ready now")
+
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        print("Downloading audio now\n")
+        ydl.download([url])
+
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            name = file
+            print(f"Renamed File: {file}\n")
+            os.rename(file, "song.mp3")
+
+    voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: print("Song done!"))
+    voice.source = discord.PCMVolumeTransformer(voice.source)
+    voice.source.volume = 0.07
+
+    nname = name.rsplit("-", 2)
+    await ctx.send(f"Playing: {nname[0]}")
+    print("playing\n")
+
+
+bot.run(TOKEN)
